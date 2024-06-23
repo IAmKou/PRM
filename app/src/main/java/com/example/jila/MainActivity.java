5a342f8f095219ab4c58aa0d37f5f56649070628
IAmKou <luongcu111@gmail.com>
2024-06-24 00:39:18 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/jila/MainActivity.java
+++ app/src/main/java/com/example/jila/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.jila;
 
+
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,19 +9,32 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         Button buttonNext = findViewById(R.id.buttonNext);
+        Button b = findViewById(R.id.btn2);
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ScreenTwoActivity.class);
                 startActivity(intent);
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ReportListActivity.class);
+                startActivity(intent);
+
             }
         });
     }
