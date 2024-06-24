a1bb7dec1a454bcffd21e2ee78aa810b470022eb
TUNG\khact <tungtkhs163077@fpt.edu.vn>
2024-06-24 19:15:39 +0700

    update login, Signup, Forget

--- app/src/main/java/com/example/jila/ForgetActivity.java
+++ app/src/main/java/com/example/jila/ForgetActivity.java
@@ -1,24 +1,77 @@
 package com.example.jila;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.jila.databinding.ActivityForgetBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ForgetActivity extends AppCompatActivity {
 
+    ActivityForgetBinding binding;
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_forget);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+       binding = ActivityForgetBinding.inflate(getLayoutInflater());
+       setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Create your account");
+        progressDialog.setMessage("Please Wait");
+
+
+        binding.forgetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.editEmailAddress.getText().toString();
+                progressDialog.dismiss();
+                if (email.isEmpty()){
+
+                    binding.editEmailAddress.setError("Enter Your Email");
+                }else{
+
+                auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+
+                        progressDialog.dismiss();
+                            Toast.makeText(ForgetActivity.this, "Please Check Your Email", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(ForgetActivity.this, LoginActivity.class));
+                        }else{
+
+                            progressDialog.dismiss();
+                            Toast.makeText(ForgetActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                }
+            }
         });
+
+       binding.login.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               startActivity(new Intent(ForgetActivity.this, LoginActivity.class));
+           }
+       });
     }
 }
--- app/src/main/java/com/example/jila/LoginActivity.java
+++ app/src/main/java/com/example/jila/LoginActivity.java
@@ -1,24 +1,103 @@
 package com.example.jila;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.jila.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class LoginActivity extends AppCompatActivity {
 
+    ActivityLoginBinding binding;
+    FirebaseAuth auth;
+    FirebaseFirestore firestore;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Create your account");
+        progressDialog.setMessage("Please Wait");
+
+        binding.newAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+
+            }
+        });
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = binding.editEmailAddress.getText().toString();
+                String password = binding.editpassword.getText().toString();
+
+
+                if(email.isEmpty()){
+
+                    binding.editEmailAddress.setError("Enter Your Email");
+                } else if (password.isEmpty()) {
+
+                    binding.editpassword.setError("Enter Your Password");
+
+                }else{
+
+                progressDialog.show();
+
+                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+
+                        progressDialog.dismiss();
+
+                        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                        finish();
+
+                        }else{
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+                }
+
+            }
+        });
+
+
+        binding.forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginActivity.this,ForgetActivity.class));
+
+
+            }
         });
     }
 }
