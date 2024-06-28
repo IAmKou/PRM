c834b2775b47e4f70625bb8132d7379166b7aead
IAmKou <luongcu111@gmail.com>
2024-06-29 00:51:43 +0700

    update

--- app/src/main/java/com/example/jila/MainActivity.java
+++ app/src/main/java/com/example/jila/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jila;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         auth = FirebaseAuth.getInstance();
+        Button b1 = findViewById(R.id.button);
+        Button b2 = findViewById(R.id.button2);
+        Button b3 = findViewById(R.id.button3);
 
         binding.changePass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +35,27 @@ public class MainActivity extends AppCompatActivity {
 
                 startActivity(new Intent(MainActivity.this, ChangeActivity.class));
 
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ReportListActivity.class);
+                startActivity(intent);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateQuizActivity.class);
+                startActivity(intent);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                startActivity(intent);
             }
         });
     }
