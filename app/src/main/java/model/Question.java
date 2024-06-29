f89d888de31507bb994e5855218bfd00bb0c05ee
IAmKou <luongcu111@gmail.com>
2024-06-29 22:00:29 +0700

    Update Question.java

--- app/src/main/java/model/Question.java
+++ app/src/main/java/model/Question.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 public class Question {
     private String question;
-    private Long question_id;
     private List<Answer> answers;
 
 
-    public Question(String question, Long question_id, List<Answer> answers) {
+    public Question(String question, List<Answer> answers) {
         this.question = question;
-        this.question_id = question_id;
         this.answers = answers;
 
     }
@@ -22,15 +20,6 @@ public class Question {
     public void setQuestion(String question) {
         this.question = question;
     }
-
-    public Long getQuestion_id() {
-        return question_id;
-    }
-
-    public void setQuestion_id(Long question_id) {
-        this.question_id = question_id;
-    }
-
     public List<Answer> getAnswers() {
         return answers;
     }
