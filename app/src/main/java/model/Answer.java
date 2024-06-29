4ee29c559a868cfb0a819ce932223ba5360985a9
IAmKou <luongcu111@gmail.com>
2024-06-29 22:00:32 +0700

    Update Answer.java

--- app/src/main/java/model/Answer.java
+++ app/src/main/java/model/Answer.java
@@ -1,25 +1,17 @@
 package model;
 
 public class Answer {
-    private Long answer_id;
+
     private String answer_text;
     private boolean is_correct;
 
 
-    public Answer(Long answer_id, String answer_text, boolean is_correct) {
-        this.answer_id = answer_id;
+    public Answer(String answer_text, boolean is_correct) {
         this.answer_text = answer_text;
         this.is_correct = is_correct;
 
     }
 
-    public Long getAnswer_id() {
-        return answer_id;
-    }
-
-    public void setAnswer_id(Long answer_id) {
-        this.answer_id = answer_id;
-    }
 
     public String getAnswer_text() {
         return answer_text;
