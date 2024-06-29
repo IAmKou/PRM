7ed67899840c7ab9d08bf3c3db17c11f77079b77
IAmKou <luongcu111@gmail.com>
2024-06-29 22:00:39 +0700

    Update ReportDetailActivity.java

--- app/src/main/java/com/example/jila/ReportDetailActivity.java
+++ app/src/main/java/com/example/jila/ReportDetailActivity.java
@@ -1,40 +1,32 @@
 package com.example.jila;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import model.Report;
+
 public class ReportDetailActivity extends AppCompatActivity {
+    public static final String EXTRA_REPORT = "com.example.jila.REPORT";
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_detail);
 
-        TextView reportIdTextView = findViewById(R.id.rid);
-        TextView reportTimeTextView = findViewById(R.id.rt);
-        TextView reporterTextView = findViewById(R.id.reid);
-        TextView quizIdTextView = findViewById(R.id.qid);
-        TextView reportTypeTextView = findViewById(R.id.tid);
+        Report report = getIntent().getParcelableExtra(EXTRA_REPORT);
 
-        // Get the data from the Intent
-        Intent intent = getIntent();
-        if (intent != null) {
-            int reportId = intent.getIntExtra("report_id", -1); // Default to -1 if not found
-            String reportTime = intent.getStringExtra("report_time");
-            String reporter = intent.getStringExtra("reporter");
-            int quizId = intent.getIntExtra("quiz_id", -1); // Default to -1 if not found
-            String reportType = intent.getStringExtra("report_type");
+        if (report != null) {
+            TextView reporterTextView = findViewById(R.id.reporter);
+            TextView typeIdTextView = findViewById(R.id.type_id);
+            TextView quizIdTextView = findViewById(R.id.quiz_id);
+            TextView reportTimeTextView = findViewById(R.id.report_time);
 
-            // Set the retrieved data to the TextViews
-            reportIdTextView.setText(String.valueOf(reportId));
-            reportTimeTextView.setText(reportTime);
-            reporterTextView.setText(reporter);
-            quizIdTextView.setText(String.valueOf(quizId));
-            reportTypeTextView.setText(reportType);
+            reporterTextView.setText(report.getReporter());
+            typeIdTextView.setText(report.getTypeName());
+            quizIdTextView.setText(report.getQuizName());
+            reportTimeTextView.setText(report.getReport_time().toDate().toString());
         }
     }
 }
