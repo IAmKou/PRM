b11619857f5ef36a2a42073c92e65233a6e2c4e7
NHMHieuA1K57 <124542355+NHMHieuA1K57@users.noreply.github.com>
2024-06-30 16:17:08 +0700

    fix_1

--- app/src/main/java/com/example/jila/MainActivity.java
+++ app/src/main/java/com/example/jila/MainActivity.java
@@ -4,20 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.jila.databinding.ActivityChangeBinding;
 import com.example.jila.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,39 +20,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         auth = FirebaseAuth.getInstance();
-        Button b1 = findViewById(R.id.button);
-        Button b2 = findViewById(R.id.button2);
-        Button b3 = findViewById(R.id.button3);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        binding.changePass.setOnClickListener(new View.OnClickListener() {
+        Button b1 = findViewById(R.id.button);
+        Button b2 = findViewById(R.id.btn2);
+        Button b3 = findViewById(R.id.button3);
+        Button changePassButton = findViewById(R.id.changePass);
+
+        changePassButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(new Intent(MainActivity.this, ChangeActivity.class));
-
             }
         });
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,ReportListActivity.class);
+                Intent intent = new Intent(MainActivity.this, ReportListActivity.class);
                 startActivity(intent);
             }
         });
+
         b2.setOnClickListener(new View.OnClickListener() {
-
-        setContentView(R.layout.activity_main);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Button b = findViewById(R.id.btn2);
-
-        b.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CreateQuizActivity.class);
                 startActivity(intent);
             }
         });
+
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
+
     public void openExam(View view) {
         startActivity(new Intent(this, ExamActivity.class));
     }
