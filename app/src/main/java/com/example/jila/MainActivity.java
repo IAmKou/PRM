b6de191469d7ee77e63aeed9e4d7a3ed62e99c5a
IAmKou <luongcu111@gmail.com>
2024-06-30 14:50:24 +0700

    fix lai main

--- app/src/main/java/com/example/jila/MainActivity.java
+++ app/src/main/java/com/example/jila/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.jila.databinding.ActivityChangeBinding;
 import com.example.jila.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -45,13 +46,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         b2.setOnClickListener(new View.OnClickListener() {
-
-        setContentView(R.layout.activity_main);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Button b = findViewById(R.id.btn2);
-
-        b.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CreateQuizActivity.class);
@@ -66,20 +60,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-    public void openExam(View view) {
-        startActivity(new Intent(this, ExamActivity.class));
-    }
-
-    public void openQuizList(View view) {
-        startActivity(new Intent(this, QuizListActivity.class));
-    }
-
-    public void openLearning(View view) {
-        startActivity(new Intent(this, LearningActivity.class));
-    }
-
-    public void openUserList(View view) {
-        startActivity(new Intent(this, UserListActivity.class));
+}
     }
 }
