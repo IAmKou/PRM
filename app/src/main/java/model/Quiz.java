e47d2a5b60ef04e5746cdeec2550684e1bb35bca
IAmKou <luongcu111@gmail.com>
2024-07-01 12:11:14 +0700

    Update Quiz.java

--- app/src/main/java/model/Quiz.java
+++ app/src/main/java/model/Quiz.java
@@ -1,47 +1,39 @@
 package model;
 
 public class Quiz {
-    private Long creator_id;
-    private Long quiz_id;
+    private String creator_name;
     private String quiz_name;
     private String type;
 
-    public Quiz(Long creator_id, Long quiz_id, String quiz_name, String type) {
-        this.creator_id = creator_id;
-        this.quiz_id = quiz_id;
+
+
+    public Quiz(String creator_name, String quiz_name, String type) {
+        this.creator_name = creator_name;
         this.quiz_name = quiz_name;
         this.type = type;
     }
 
-    public void setCreator_id(Long creator_id) {
-        this.creator_id = creator_id;
+    public String getCreator_name() {
+        return creator_name;
     }
 
-    public void setQuiz_id(Long quiz_id) {
-        this.quiz_id = quiz_id;
-    }
-
-    public void setQuiz_name(String quiz_name) {
-        this.quiz_name = quiz_name;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Long getCreator_id() {
-        return creator_id;
-    }
-
-    public Long getQuiz_id() {
-        return quiz_id;
+    public void setCreator_name(String creator_name) {
+        this.creator_name = creator_name;
     }
 
     public String getQuiz_name() {
         return quiz_name;
     }
 
+    public void setQuiz_name(String quiz_name) {
+        this.quiz_name = quiz_name;
+    }
+
     public String getType() {
         return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
