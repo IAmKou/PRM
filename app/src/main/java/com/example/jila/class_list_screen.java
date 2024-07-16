06f9aff3213f0636b7d5b1ef532e9dfdcd8f4399
Whatever0313 <105837211+nguyenhoanghuy313@users.noreply.github.com>
2024-07-17 01:08:16 +0700

    class list

--- app/src/main/java/com/example/jila/class_list_screen.java
+++ app/src/main/java/com/example/jila/class_list_screen.java
@@ -1,14 +1,28 @@
 package com.example.jila;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class class_list_screen extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private RecyclerView recyclerView;
+    private QuizAdapter quizAdapter;
+    private List<Class> classList = new ArrayList<>();
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +30,13 @@ public class class_list_screen extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_class_list_screen);
 
+        db = FirebaseFirestore.getInstance();
+        recyclerView = findViewById(R.id.RecyclerViewClass);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        quizAdapter = new QuizAdapter(this, classList);
+//        recyclerView.setAdapter(quizAdapter);
+//
+//        backButton = findViewById(R.id.backButton);
+//        backButton.setOnClickListener(v -> finish());
     }
 }
