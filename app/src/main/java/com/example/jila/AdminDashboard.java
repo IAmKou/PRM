426607ee1defefdbc4d0fe9bea68a39f2ffe92ab
Whatever0313 <105837211+nguyenhoanghuy313@users.noreply.github.com>
2024-07-17 09:21:48 +0700

    admin dashboard

--- app/src/main/java/com/example/jila/AdminDashboard.java
+++ app/src/main/java/com/example/jila/AdminDashboard.java
@@ -1,6 +1,9 @@
 package com.example.jila;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,33 @@ public class AdminDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button button1 = findViewById(R.id.UserList_btn);
+        Button button2 = findViewById(R.id.Q_btn);
+        Button button3 = findViewById(R.id.rp_btn);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminDashboard.this, Quiz.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminDashboard.this, ReportListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminDashboard.this, UserListActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
